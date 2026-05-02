0e9c37a60dbec06702dffdc7a1436e1859067f29
Aral Yılmaz <aralyilmaz1@outlook.com>
2026-05-02 13:54:44 +0300

    implemented 7seg

--- verilog/7seg.v
+++ verilog/7seg.v
@@ -1,13 +1,34 @@
 module tb_sevenSeg;
-    wire O;
+    wire A, B, C, D, E, F, G;
     wire [3:0] I;
 
-    aSeg a(O, I);
-    aSeg_test ta(O, I);
+    sevenSeg s(A, B, C, D, E, F, G, I);
+    test_all ta(A, B, C, D, E, F, G, I);
 
 endmodule
 
-module aSeg_test(
+module test_all(
+    input A, B, C, D, E, F, G,
+    output reg [3:0] I
+);
+
+    integer i;
+
+    initial begin
+        $monitor("I = %d A = %b B = %b C = %b D = %b E = %b F = %b G = %b", I, A, B, C, D, E, F, G);
+
+        I = 0;
+
+        for (i = 0; i < 16; i = i + 1) begin
+            #1 I = i;
+        end
+
+        #1 $finish;
+    end
+
+endmodule
+/*
+module Seg_test(
     input O,
     output reg [3:0] I
 );
@@ -15,19 +36,19 @@ module aSeg_test(
     integer i;
 
     initial begin
-        $monitor("I = %d A = %b", I, O);
+        $monitor("I = %d O = %b", I, O);
 
         I = 0;
     
-        for (i = 0; i < 16; i = i + 1) begin
+        for (i = 0; i < 10; i = i + 1) begin
             #1 I = i;
         end
 
-        $finish;
+        #1 $finish;
     end
 
 endmodule
-
+*/
 module aSeg(
     output O,
     input [3:0] I
@@ -42,16 +63,56 @@ module bSeg(
     input [3:0] I
 );
 
-    assign O = I[3] | I[2] | (I[1] ~^ I[0]);
+    assign O = I[3] | ~I[2] | (I[1] ~^ I[0]);
 
 endmodule
-/*
+
 module cSeg(
     output O,
     input [3:0] I
 );
 
-    
+    assign O = I[2] | ~I[1] | I[0];
+
+endmodule
+
+module dSeg(
+    output O,
+    input [3:0] I
+);
+
+    assign O = I[3] | (~I[2] | I[1] | I[0]) & (~I[0] | I[2] ^ I[1]);
+
+endmodule
+
+module eSeg(
+    output O,
+    input [3:0] I
+);
+
+    assign O = ~I[0] & (I[1] | ~I[2]);
+
+endmodule
+
+module fSeg(
+    output O,
+    input [3:0] I
+);
+
+    assign O = ~I[1] & (I[3] | I[2]) | ~I[0] & (I[2] | ~I[1]);
+
+endmodule
+
+module gSeg(
+    output O,
+    input [3:0] I
+);
+
+    or
+        o0(M0, I[3], I[2], I[1]),
+        o1(M1, ~I[2], ~I[1], ~I[0]);
+    and
+        a0(O, M0, M1);
 
 endmodule
 
@@ -59,4 +120,24 @@ module sevenSeg(
     output wire A, B, C, D, E, F, G,
     input [3:0] I
 );
-endmodule*/
+    wire dontCare;
+    
+    assign dontCare = ~I[3] | (~I[2] & ~I[1]);
+
+    aSeg a(tmpA, I);
+    bSeg b(tmpB, I);
+    cSeg c(tmpC, I);
+    dSeg d(tmpD, I);
+    eSeg e(tmpE, I);
+    fSeg f(tmpF, I);
+    gSeg g(tmpG, I);
+
+    assign A = tmpA & dontCare;
+    assign B = tmpB & dontCare;
+    assign C = tmpC & dontCare;
+    assign D = tmpD & dontCare;
+    assign F = tmpF & dontCare;
+    assign E = tmpE & dontCare;
+    assign G = tmpG & dontCare;
+
+endmodule
